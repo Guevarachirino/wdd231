467dabba00f3427728bdc2337f07893bb67bb917
Guevarachirino <chi23017@byui.edu>
2025-05-20 00:29:10 -0400

    new

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -53,3 +53,21 @@ listBtn.addEventListener("click", () => {
 });
 
 loadMembers();
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+//footer
+const getyear = document.getElementById('year');
+const actualyear = new Date().getFullYear();
+getyear.textContent = actualyear;
+
+
+let text = document.lastModified;
+document.getElementById("oLastModif").innerHTML = text;
