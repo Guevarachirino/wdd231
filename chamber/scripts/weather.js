60894fb4fea505f8565e9f04eb913e21aee5c365
Guevarachirino <chi23017@byui.edu>
2025-05-26 00:35:20 -0400

    new

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,5 +1,5 @@
 //select the html elements in the documents
-const myTown = document.querySelector('#own');
+const myTown = document.querySelector('#town');
 const myDescription = document.querySelector('#description');
 const myTemperature = document.querySelector('#temperature');
 const myGraphic = document.querySelector('#graphic');
