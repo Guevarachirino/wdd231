9d0eb5da03ccbb61c9f7706f93997b6e3524e853
Guevarachirino <chi23017@byui.edu>
2025-06-01 21:10:55 -0400

    new

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -37,7 +37,7 @@ if (window.location.pathname.includes("thankyou.html")) {
     const params = new URLSearchParams(window.location.search);
 
     document.querySelector('#thankyouresult').innerHTML = `
-      <h2>Your Submission</h2>
+      <h2>Congratulations, Thank you for being part of our group!</h2>
       <p><strong>First Name:</strong> ${params.get('first')}</p>
       <p><strong>Last Name:</strong> ${params.get('last')}</p>
       <p><strong>Email:</strong> ${params.get('email')}</p>
