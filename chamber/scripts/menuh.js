9faa475ee7eb1b9a5cdc7e8f9e5033686b4cbb39
Guevarachirino <chi23017@byui.edu>
2025-06-08 21:29:51 -0400

    new

--- chamber/scripts/menuh.js
+++ chamber/scripts/menuh.js
@@ -6,3 +6,11 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+// fecha de modificacion
+const getyear = document.getElementById('year');
+const actualyear = new Date().getFullYear();
+getyear.textContent = actualyear;
+
+
+let text = document.lastModified;
+document.getElementById("oLastModif").innerHTML = text;
